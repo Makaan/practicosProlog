08731459f5498aa15e0dbd856c27ac99332e8690
Makaan <nestor.figliuolo@gmail.com>
2018-04-05 15:40:00 -0300

    asd

--- PB.pl
+++ PB.pl
@@ -42,4 +42,43 @@ buscar(A, [_ | L]):- buscar(A, L).
 invertir([], []).
 invertir([X | L], LR):- invertir(L, LRI), addLast(X, LRI, LR).
 
-  
+% f)
+    %1)
+    borrarUnElemento(A, [A | L], L).
+    borrarUnElemento(A, [X | L], [X | LRes]):- borrarUnElemento(A, L, LRes). 
+    
+    %2)
+    borrarTodos(_, [], []).
+    borrarTodos(A, [A | L], LRes):- borrarTodos(A, L, LRes).
+    borrarTodos(A, [X | L], [X | LRes]):- X\=A, borrarTodos(A, L, LRes).
+
+% g)
+    %1)
+    cambiarUnElemento(A, B, [B | L], [A | L]).
+    cambiarUnElemento(A, B, [X | L], [X | LRes]):- cambiarUnElemento(A, B, L, LRes).
+
+    %2)
+    cambiarTodos(A, B, [], []).
+    cambiarTodos(A, B, [B | L], [A | LRes]):- cambiarTodos(A, B, L, LRes).
+    cambiarTodos(A, B, [X | L], [X | LRes]):- X \= B, cambiarTodos(A, B, L, LRes).
+
+% h)
+listasIguales([], []).
+listasIguales([A | L1], [A | L2]):- listasIguales(L1, L2).
+
+palindromo(L):- invertir(L, LI), listasIguales(L, LI).
+
+% i)
+listaDoble(L, LRes):- invertir(L, LI), concatenar(L, LI, LRes).
+
+% j)
+moverUnoADerecha([], []).
+moverUnoADerecha(L, LRes):- moverUnoADerechaAux(L, LU, U), concatenar([U], LU, LRes).
+
+moverUnoADerechaAux([U | []], [] , U).
+moverUnoADerechaAux([X | L], [X | LU] , U):- moverUnoADerechaAux(L,LU, U).
+
+% k)
+moverUnoAIzquierda([], []).
+moverUnoAIzquierda([X | L], LRes):- concatenar(L, [X], LRes).
+
